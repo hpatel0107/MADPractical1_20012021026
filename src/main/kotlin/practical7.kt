fb4e4bb7d2e78b8dc5dd67acc97b8a7d5d7617a5
hpatel0107 <neehopa@gmail.com>
2022-08-10 18:32:35 +0530

    started practical2

---
+++ src/main/kotlin/practical7.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    print("Enter a  integer number: ")
+    val number= readLine()!!.toInt()
+    val factorial = fact(number)
+    println("Factorial of $number = $factorial")
+}
+
+fun fact(num: Int): Int {
+    return if(num == 1){
+        num
+    }
+    else{
+        num*fact(num-1)
+    }
+}
